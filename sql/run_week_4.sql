0e9b2eb73307801da9dd46898817338da614f3bc
Michael <9612057+mikl-vb@users.noreply.github.com>
2023-11-29 18:21:15 -0800

    Initial commit

---
+++ sql/run_week_4.sql
@@ -0,0 +1,12 @@
+.output answers/week-4.txt
+.echo on
+.print nyc_transit/target/compiled/nyc_transit/analyses/bike_trips_and_duration_by_weekday.sql
+.read nyc_transit/target/compiled/nyc_transit/analyses/bike_trips_and_duration_by_weekday.sql
+.print nyc_transit/target/compiled/nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+.read nyc_transit/target/compiled/nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+.print nyc_transit/target/compiled/nyc_transit/analyses/inter_borough_taxi_trips_by_weekday2.sql
+.read nyc_transit/target/compiled/nyc_transit/analyses/inter_borough_taxi_trips_by_weekday2.sql
+.print nyc_transit/target/compiled/nyc_transit/analyses/inter_borough_taxi_trips_by_weekday3.sql
+.read nyc_transit/target/compiled/nyc_transit/analyses/inter_borough_taxi_trips_by_weekday3.sql
+.print nyc_transit/target/compiled/nyc_transit/analyses/taxi_trips_ending_at_airport.sql
+.read nyc_transit/target/compiled/nyc_transit/analyses/taxi_trips_ending_at_airport.sql
